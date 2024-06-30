de749af30dab1b5d5f6be5a5303adf120dbed6be
Anthony <ndegwaanthony300@gmail.com>
2024-06-30 23:32:04 +0300

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """
-0-main
+Pascal triangle formation
 """
-pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
 
 
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
+def pascal_triangle(n):
+    if n <= 0:
+        return []
 
+    triangle = [[1]]
 
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
