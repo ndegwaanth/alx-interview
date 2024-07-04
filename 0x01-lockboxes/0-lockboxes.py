f8f1769c041e42dde6e9e6129f2dbafaf3daf535
Anthony <ndegwaanthony300@gmail.com>
2024-07-04 22:25:33 +0300

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
+""" lockbox module"""
+
 
 def canUnlockAll(boxes):
-    """
-    method that determines if all the boxes can be opened.
-    arg:
-        boxes: This are box to be checked.
-    """
-    n = len(boxes)  # Number of boxes
-    opened = [False] * n  # Track which boxes have been opened
-    opened[0] = True  # The first box is always open
-    stack = [0]  # Start with the first box
-    visited = set()  # To keep track of visited boxes
-
+    """Determines if all boxes can be unlocked from box 0."""
+    stack = [0]
+    visited = [False for i in range(len(boxes))]
+    visited[0] = True
     while stack:
         current_box = stack.pop()
-        if current_box in visited:
-            continue
-        visited.add(current_box)
-
+        # Iterate through keys in the current box
         for key in boxes[current_box]:
-            if key < n and not opened[key]:
-                opened[key] = True
+            if 0 <= key < len(boxes) and not visited[key]:
+                visited[key] = True
                 stack.append(key)
-
-    return all(opened)
+    return False not in visited
