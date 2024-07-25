a3baafe2bbb0e7330ab06c7475a8a3e7205daf31
Anthony <ndegwaanthony300@gmail.com>
2024-07-26 00:50:47 +0300

    0. UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,59 +4,27 @@
 
 def validUTF8(data):
     """
-     determines if a given data set represents
-     a valid UTF-8 encoding.
-     parameters:
-     data: this is the list represents the string
+    determines if a given data set represents
+    a valid UTF-8 encoding.
     """
-    def is_valid_byte(byte, pattern):
-        """
-        This check if the byte match the pattern
-        parameters:
-        byte: bytes that are being checked
-        pattern: the paradigm tha it must follow
-        return: a boolean either True or False
-        """
-        return (byte & pattern[0]) == pattern[1]
+    num_bytes = 0
 
-    # Patterns for UTF-8 encoding:
-    # 0xxxxxxx for 1-byte (ASCII)
-    # 110xxxxx for 2-byte start
-    # 11110xxx for 4-byte start
-    # 10xxxxxx for continuation
-    patterns = [
-        (0b10000000, 0b00000000),
-        (0b11100000, 0b11000000),
-        (0b11110000, 0b11100000),
-        (0b11111000, 0b11110000),
-        (0b11000000, 0b10000000)
-    ]
-
-    n = len(data)
-    i = 0
-
-    while i < n:
-        byte = data[i]
+    for byte in data:
         if byte > 255:
             return False
 
-        # Determine the number of bytes in the character
-        if is_valid_byte(byte, patterns[0]):
-            i += 1
-            continue
-        elif is_valid_byte(byte, patterns[1]):
-            num_bytes = 2
-        elif is_valid_byte(byte, patterns[2]):
-            num_bytes = 3
-        elif is_valid_byte(byte, patterns[3]):
-            num_bytes = 4
-        else:
-            return False
-
-        for j in range(1, num_bytes):
-            if i + j >= n or not is_valid_byte(data[i + j], patterns[4]):
+        if num_bytes == 0:
+            if (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            elif (byte >> 7) != 0:
                 return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            num_bytes -= 1
 
-        i += num_bytes
-
-    return True
+    return num_bytes == 0
